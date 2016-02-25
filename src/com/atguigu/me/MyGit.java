79a02e1aee76cecbd74606a3e323b2ef8ba2767b
yangrui01 <836076257@qq.com>
2016-02-25 11:49:44 +0800

    update 01
    update 01

--- src/com/atguigu/me/MyGit.java
+++ src/com/atguigu/me/MyGit.java
@@ -4,5 +4,7 @@ public class MyGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello man !");
+		System.out.println("hello man2 !");
+		System.out.println("hello man3 !");
 	}  
 }
